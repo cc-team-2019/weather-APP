fc09a62fd3be38f3203f5558afc0ebe6803bb83e
wallgravve <pawlaczyk.lukasz@gmail.com>
2019-08-28 21:45:47 +0200

    Fix axios, hide key

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-// import Weather from './modules/weather.js';
-
+const axios = require('axios');
     
-
+let lat, long;
      if ('geolocation' in navigator) {
          console.log('geolocation available');
          navigator.geolocation.getCurrentPosition(position => {
-             const lat = (position.coords.latitude);
-             const long = (position.coords.longitude);
-             document.getElementById('latitude').textContent = lat;
-             document.getElementById('longitude'). textContent = long;
-   console.log(position)
-            
+             lat = (position.coords.latitude);
+             long = (position.coords.longitude);
+             document.getElementById('latitude').textContent = lat.toFixed(2);
+             document.getElementById('longitude'). textContent = long.toFixed(2);
+             console.log(position);
+             const API_KEY = '54ab60394b58b2041b7d11706841f562'
+             const ROOT_URL = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${API_KEY}/`;
+      
+       
  });
  
      } else {
          console.log('geolocation not available');
      }
+
+
+
+    
+     
+
+axios.get(`${ROOT_URL}/${lat},${long}`, {
+  params: {
+      ID: 12345
+  }
+})
+.then((response) => {
+    return response;
+    console.log(response)
+})
+.catch((error) => {
+    console.log(error);
+});
