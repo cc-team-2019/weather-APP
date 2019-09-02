74b191c6a407afee0578c32a3c86a1c416b3d75c
danielszymak <danielszymak@gmail.com>
2019-09-02 11:29:02 +0200

    basic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './css/style.css';
+
 const axios = require('axios');
 const API_KEY = '76d4597073b97b1d63fb3d8ec724ef57';
 const ROOT_URL = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${API_KEY}/`;
@@ -37,5 +39,25 @@ let long;
      } else {
          console.log('geolocation not available');
      }
+var button1 = document.getElementById("showWeather").addEventListener('click', buttonClick);
+
+function buttonClick(){
+    document.querySelector('.chooseCity').style.display= 'none';
+    document.querySelector('.weatherPresentation').style.position= 'fixed';
+}
+     
+var button2 = document.getElementById("weatherHourTitle").addEventListener('click', button2Click);
+function button2Click(){
+    document.querySelector('.weatherPresentation').style.position='relative';
+    document.querySelector('.weatherWeek section').style.display='none';
+    document.querySelector('.weatherHour section').style.display='grid';
+}
+     
+var button3 = document.getElementById("weatherWeekTitle").addEventListener('click', button3Click);
+function button3Click(){
+    document.querySelector('.weatherPresentation').style.position='relative';
+    document.querySelector('.weatherWeek section').style.display='grid';
+    document.querySelector('.weatherHour section').style.display='none';
+}
 
 
