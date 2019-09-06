ef666b309cc8f4b69a9f9c5660a8121d362a0a3e
danielszymak <danielszymak@gmail.com>
2019-09-07 00:03:28 +0200

    commit_with_speechsynthesis

--- src/index.js
+++ src/index.js
@@ -149,3 +149,148 @@ for (let i = 0; i < arr_days.length; i++) {
          document.querySelector('.chooseCity').style.position='fixed';
          document.querySelector('.chooseCity').style.display= 'block';
      }
+
+     var button5 = document.getElementById("settings").addEventListener('click', button5Click);
+
+     function button5Click(){
+         document.getElementById('settings').style.backgroundColor='white';
+         document.getElementById('settings').style.borderRadius='20px';
+         document.getElementById('settings').style.width='90vw';
+         document.getElementById('settings').style.height='40vh';
+         document.getElementById('settings').style.border="2px solid gold"
+         let setting = document.getElementById('settings').children;
+            for (let a=0; a < setting.length; a++) {
+            setting[a].style.display='flex';
+            setting[a].style.padding='0 10px';
+            setting[a].style.alignSelf="center";
+            setting[a].style.justifyContent="space-between";
+            }
+         document.getElementById('accept').style.width='100%';
+         document.getElementById('accept').style.justifySelf='center';
+     }
+
+     var button6 = document.getElementById('accept').addEventListener('click', button6Click);
+     function button6Click(e){
+         e.stopPropagation();
+         let setting = document.getElementById('settings').children;
+         for (let a=0; a < setting.length; a++) {
+             setting[a].style.display='none';
+         }
+        document.getElementById('settings').style.removeProperty('background-color');
+        document.getElementById('settings').style.borderRadius='0';
+        document.getElementById('settings').style.width='30px';
+        document.getElementById('settings').style.height='30px';
+        document.getElementById('settings').style.border="0"
+}
+
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+msg.text = "Witaj w aplikacji maj weather. Podaj nazwę miasta";
+msg.pitch = 1;
+msg.rate = 1;
+msg.volume = 1;
+msg.lang = 'pl'
+function populateVoices() {
+    voices = this.getVoices();
+    console.log(voices);
+    for (let i = 0; i < voices.length; i++) {
+        if (voices[i].lang==="pl-PL") {
+            msg.voice = voices[i]
+            break;
+        }
+    }
+}
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+const msg2 = new SpeechSynthesisUtterance();
+msg2.pitch = 1;
+msg2.rate = 1;
+msg2.volume = 1;
+msg2.lang = 'pl'
+msg2.voice = msg.voice;
+
+const msg3 = new SpeechSynthesisUtterance();
+msg3.pitch = 1;
+msg3.rate = 1;
+msg3.volume = 1;
+msg3.lang = 'pl'
+msg3.voice = msg.voice;
+
+const msg4 = new SpeechSynthesisUtterance();
+msg4.pitch = 1;
+msg4.rate = 1;
+msg4.volume = 1;
+msg4.lang = 'pl'
+msg4.voice = msg.voice;
+
+const msg5 = new SpeechSynthesisUtterance();
+msg5.pitch = 1;
+msg5.rate = 1;
+msg5.volume = 1;
+msg5.lang = 'pl'
+msg5.voice = msg.voice;
+
+const msg6= new SpeechSynthesisUtterance();
+msg6.pitch = 1;
+msg6.rate = 1;
+msg6.volume = 1;
+msg6.lang = 'pl'
+msg6.voice = msg.voice;
+
+const msg7 = new SpeechSynthesisUtterance();
+msg7.pitch = 1;
+msg7.rate = 1;
+msg7.volume = 1;
+msg7.lang = 'pl'
+msg7.voice = msg.voice;
+
+const msg8 = new SpeechSynthesisUtterance();
+msg8.pitch = 1;
+msg8.rate = 1;
+msg8.volume = 1;
+msg8.lang = 'pl'
+msg8.voice = msg.voice;
+
+const msg9 = new SpeechSynthesisUtterance();
+msg9.pitch = 1;
+msg9.rate = 1;
+msg9.volume = 1;
+msg9.lang = 'pl'
+msg9.voice = msg.voice;
+     
+function speak() {
+    if (document.getElementById('readText').checked === true) {
+        speechSynthesis.speak(msg);
+    }
+}
+     
+document.querySelector('.chooseCity').addEventListener('click', speak);
+     
+function speak2() {
+    if (document.getElementById('readText').checked === true) {
+        msg2.text = document.querySelector('#cityName').textContent;
+        speechSynthesis.speak(msg2);
+        msg3.text = msg3.text = document.querySelector('#datefull').textContent;
+        speechSynthesis.speak(msg3);
+        msg4.text = 'Temperatura' + document.querySelector('.temperature-block').textContent + 'stopni Celsjusza';
+        speechSynthesis.speak(msg4);
+        msg5.text = document.querySelector('.precipitation-block').textContent;
+        speechSynthesis.speak(msg5);
+        msg6.text = document.querySelector('.wind-block').textContent;
+        speechSynthesis.speak(msg6);
+        msg7.text = document.querySelector('.pressure-block').textContent;
+        speechSynthesis.speak(msg7);
+        msg8.text = document.querySelector('#sunrise').textContent;
+        speechSynthesis.speak(msg8);
+        msg9.text = document.querySelector('#sunset').textContent;
+        speechSynthesis.speak(msg9);   
+    }
+}
+
+function stop() {
+    speechSynthesis.cancel();
+}
+     
+document.querySelector('.weatherPresentation').addEventListener('click', speak2);
+
--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ var button1 = document.getElementById("showWeather").addEventListener('click', b
 function buttonClick(){
     document.querySelector('.chooseCity').style.display= 'none';
     document.querySelector('.weatherPresentation').style.position= 'fixed';
+    document.getElementById('settings').style.display='grid';
 }
 
 var button2 = document.getElementById("weatherHourTitle").addEventListener('click', button2Click);
@@ -26,3 +27,88 @@ function button4Click(){
     document.querySelector('.chooseCity').style.position='fixed';
     document.querySelector('.chooseCity').style.display= 'block';
 }
+
+var button5 = document.getElementById("settings").addEventListener('click', button5Click);
+function button5Click(){
+    document.getElementById('settings').style.backgroundColor='white';
+    document.getElementById('settings').style.borderRadius='20px';
+    document.getElementById('settings').style.width='90vw';
+    document.getElementById('settings').style.height='40vh';
+    document.getElementById('settings').style.border="2px solid gold"
+    let setting = document.getElementById('settings').children;
+    for (let a=0; a < setting.length; a++) {
+        setting[a].style.display='flex';
+        setting[a].style.padding='0 10px';
+        setting[a].style.alignSelf="center";
+        setting[a].style.justifyContent="space-between";
+    }
+    document.getElementById('accept').style.width='100%';
+    document.getElementById('accept').style.justifySelf='center';
+}
+
+var button6 = document.getElementById('accept').addEventListener('click', button6Click);
+function button6Click(e){
+    e.stopPropagation();
+    let setting = document.getElementById('settings').children;
+    for (let a=0; a < setting.length; a++) {
+        setting[a].style.display='none';
+    }
+    document.getElementById('settings').style.removeProperty('background-color');
+    document.getElementById('settings').style.borderRadius='0';
+    document.getElementById('settings').style.width='30px';
+    document.getElementById('settings').style.height='30px';
+    document.getElementById('settings').style.border="0"
+}
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+msg.text = "Witaj w aplikacji maj weather. Podaj nazwę miasta";
+msg.pitch = 1;
+msg.rate = 1;
+msg.volume = 1;
+msg.lang = 'pl'
+
+function populateVoices() {
+    voices = this.getVoices();
+    console.log(voices);
+    for (let i = 0; i < voices.length; i++) {
+        if (voices[i].lang==="pl-PL") {
+            msg.voice = voices[i]
+            break;
+        }
+    }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+function speak() {
+    if (document.getElementById('readText').checked === true) {
+        speechSynthesis.speak(msg);
+    }
+}
+
+document.querySelector('.chooseCity').addEventListener('click', speak);
+
+function speak2() {
+    if (document.getElementById('readText').checked === true) {
+        let msg2 = msg;
+        msg2.text = document.querySelector('#cityName').textContent;
+        speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('#datefull').textContent;
+        //speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('.temperature-block').textContent + 'stopni Celsjusza';
+        //speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('.precipitation-block').textContent;
+        //speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('.wind-block').textContent;
+        //speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('.pressure-block').textContent;
+        //speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('#sunrise').textContent;
+        //speechSynthesis.speak(msg2);
+        //msg2.text = document.querySelector('#sunset').textContent;
+        //speechSynthesis.speak(msg2);
+    }
+}
+
+document.querySelector('.weatherPresentation').addEventListener('click', speak2);
